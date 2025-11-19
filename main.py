09c3ddd0b604121fd5813aca6cf36df5a70cd78b
Francesco Frassinelli <francesco.frassinelli@nina.no>
2025-11-20 00:24:56 +0100

    Fetch traffic data

--- main.py
+++ main.py
@@ -4,12 +4,17 @@
 
 import logging
 import pathlib
+from typing import Any
 
 import dlt
 import environ
-from dlt.extract.source import DltSource
-from dlt.sources.rest_api import rest_api_source
-from dlt.sources.rest_api.typing import RESTAPIConfig
+import tqdm
+from dlt.sources.helpers.requests import Request, Response
+from dlt.sources.helpers.rest_client.client import RESTClient
+from dlt.sources.helpers.rest_client.paginators import (
+    BasePaginator,
+    SinglePagePaginator,
+)
 
 env = environ.Env()
 BASE_DIR = pathlib.Path(__file__).parent
@@ -21,70 +26,161 @@ logging.basicConfig(level=(logging.DEBUG if DEBUG else logging.INFO))
 
 logger = logging.getLogger(__name__)
 
+client = RESTClient(
+    base_url="https://trafikkdata-api.atlas.vegvesen.no/",
+    headers={"User-Agent": "NINAnor-traffikdata-sync/0.1"},
+)
 
-@dlt.source()
-def traffikdata_source() -> DltSource:
-    config: RESTAPIConfig = {
-        "client": {
-            "base_url": "https://trafikkdata-api.atlas.vegvesen.no/",
-        },
-        "resource_defaults": {
-            "endpoint": {
-                "method": "POST",
-            },
-            "write_disposition": "merge",
-        },
-        "resources": [
-            {
-                "name": "traffic_registration_points",
-                "endpoint": {
-                    "path": "",
-                    "data_selector": "data.trafficRegistrationPoints",
-                    "paginator": {
-                        "type": "single_page",
-                    },
-                    "json": {
-                        "query": """
-                            {{
-                                trafficRegistrationPoints(
-                                    searchQuery: {{ isOperational: true }},
-                                ) {{
-                                    id
-                                    name
-                                    location {{
-                                        coordinates {{
-                                            latLon {{
-                                                lat
-                                                lon
-                                            }}
-                                        }}
-                                    }}
-                                    trafficRegistrationType
-                                    dataTimeSpan {{
-                                        firstData
-                                        firstDataWithQualityMetrics
-                                    }}
-                                    operationalStatus
-                                    registrationFrequency
-                                }}
-                            }}
-                        """,
-                    },
-                },
-                "primary_key": "id",
-            },
-        ],
+FROM_DATE = "2009-01-01T00:00:00Z"
+TO_DATE = "2026-01-01T00:00:00Z"
+
+TRAFFIC_REGISTRATION_POINTS_QUERY = """
+{
+  trafficRegistrationPoints(searchQuery: {isOperational: true}) {
+    id
+    name
+    location {
+      coordinates {
+        latLon {
+          lat
+          lon
+        }
+      }
     }
-    return rest_api_source(config)
+    trafficRegistrationType
+    dataTimeSpan {
+      firstData
+      firstDataWithQualityMetrics
+    }
+    operationalStatus
+    registrationFrequency
+  }
+}
+"""
+
+TRAFFIC_DATA_QUERY = """
+query TrafficData(
+    $point_id: String!,
+    $from: ZonedDateTime!,
+    $to: ZonedDateTime!,
+    $after: String
+) {
+  trafficData(trafficRegistrationPointId: $point_id) {
+    volume {
+      byHour(from: $from, to: $to, after: $after) {
+        pageInfo {
+          endCursor
+        }
+        edges {
+          node {
+            from
+            to
+            total {
+              volumeNumbers {
+                volume
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+"""
+
+
+class VegvesenGraphQLPaginator(BasePaginator):
+    def __init__(self) -> None:
+        super().__init__()
+        self.cursor = None
+
+    def update_state(self, response: Response, data: list[Any] | None = None) -> None:
+        page_info = response.json()["data"]["trafficData"]["volume"]["byHour"][
+            "pageInfo"
+        ]
+        self.cursor = page_info["endCursor"]
+        self._has_next_page = self.cursor is not None
+
+    def update_request(self, request: Request) -> None:
+        request.json["variables"]["after"] = self.cursor  # pyright: ignore[reportOptionalSubscript]
+
+
+@dlt.resource(
+    primary_key="id",
+    write_disposition="merge",
+)
+def traffic_registration_points():
+    yield from client.paginate(
+        path="",
+        method="POST",
+        json={"query": TRAFFIC_REGISTRATION_POINTS_QUERY},
+        data_selector="data.trafficRegistrationPoints",
+        paginator=SinglePagePaginator(),
+    )
+
+
+@dlt.resource(
+    primary_key=[
+        "traffic_registration_point_id",
+        "from",
+        "to",
+    ],
+    references=[
+        {
+            "referenced_table": "traffic_registration_points",
+            "columns": ["traffic_registration_point_id"],
+            "referenced_columns": ["id"],
+        }
+    ],
+    write_disposition="merge",
+)
+def traffic_data(point_id: str, from_timestamp: str, to_timestamp: str):
+    pages = client.paginate(
+        path="",
+        method="POST",
+        paginator=VegvesenGraphQLPaginator(),
+        data_selector="data.trafficData.volume.byHour.edges[*].node",
+        json={
+            "variables": {
+                "point_id": point_id,
+                "from": from_timestamp,
+                "to": to_timestamp,
+                "after": None,
+            },
+            "query": TRAFFIC_DATA_QUERY,
+        },
+    )
+    for page in pages:
+        for record in page:
+            record["traffic_registration_point_id"] = point_id
+            volumeNumbers = record.pop("total")["volumeNumbers"]
+            if volumeNumbers is None:
+                record["volume"] = None
+            else:
+                record["volume"] = volumeNumbers["volume"]
+            yield record
+
+
+registration_points_ids = []
+
+
+def capture_ids(record: dict[str, Any]) -> dict[str, Any]:
+    global registration_points_ids
+    registration_points_ids.append(record["id"])
+    return record
 
 
 def start() -> None:
     pipeline = dlt.pipeline(
-        pipeline_name="traffikdata_pipeline",
+        pipeline_name="trafficdata_sync",
         destination="duckdb",
-        dataset_name="traffikdata",
+        dataset_name="trafficdata",
     )
-    _load_info = pipeline.run(traffikdata_source())
+    pipeline.run(traffic_registration_points().add_map(capture_ids))
+    for point_id in tqdm.tqdm(registration_points_ids):
+        logger.debug(f"Fetching traffic data for registration point ID: {point_id}")
+        info = pipeline.run(traffic_data(point_id, FROM_DATE, TO_DATE))
+        logger.debug(f"{info}")
 
 
 if __name__ == "__main__":
